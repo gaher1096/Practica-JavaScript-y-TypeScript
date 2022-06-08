009c119f1f6b0038842c9fcbdf841718482757b8
gaher1096 <gaher1096@hotmail.com>
2022-06-08 15:41:31 -0500

    practica TypeScript: Enums y Objetos

---
+++ TypeScript/src/enums_objects.ts
@@ -0,0 +1,17 @@
+//Enum
+enum Roles{
+    User="USER",
+    Admin="ADMIN",
+    SuperAdmin="SUPERADMIN"
+};
+
+console.log(Roles.Admin);
+
+//Objects
+const roles = {
+    User: 0,
+    Audmin: 1,
+    SuperAdmin: 2
+};
+
+console.log(roles.User);
