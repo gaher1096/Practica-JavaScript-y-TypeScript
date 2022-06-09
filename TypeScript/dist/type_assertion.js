26a4695fd70762b180ff5d43d96b6727bf4ac375
gaher1096 <gaher1096@hotmail.com>
2022-06-09 09:34:18 -0500

    TypeScript: Assertion

---
+++ TypeScript/dist/type_assertion.js
@@ -0,0 +1,6 @@
+"use strict";
+let channel = "Esto es un string";
+//let channelStr = <string>channel;
+let channelStr = channel;
+console.log(channel);
+console.log(channelStr);
---
+++ TypeScript/src/type_assertion.ts
@@ -0,0 +1,7 @@
+let channel:any="Esto es un string";
+
+//let channelStr = <string>channel;
+let channelStr = channel as string;
+
+console.log(channel);
+console.log(channelStr);
